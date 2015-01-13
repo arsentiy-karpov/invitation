90af95f2042a97e79051cf5e6ce44e9f1feb1ec4
a.karpov <arsentiy.karpov@qiwi.ru>
2015-01-13 12:45:18 +0000

    Login screen

---
+++ app/src/main/java/wedding/karpov/invitation/LoginScreenGenerator.java
@@ -0,0 +1,31 @@
+package wedding.karpov.invitation;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by akarpov on 1/13/15.
+ */
+public class LoginScreenGenerator implements OverlappingScreen.InformationScreenGenerator {
+
+    @Override
+    public View getView(Context context, final OverlappingScreen overlappingInformationScreen) {
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = layoutInflater.inflate(R.layout.view_login, null);
+        v.findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                overlappingInformationScreen.detach();
+            }
+        });
+        Drawable background = context.getResources().getDrawable(R.drawable.login_background);
+        background.setAlpha(80);
+        ((ImageView)v.findViewById(R.id.login_background)).setBackgroundDrawable(background);
+        return v;
+    }
+
+}
---
+++ app/src/main/java/wedding/karpov/invitation/OverlappingScreen.java
@@ -0,0 +1,131 @@
+package wedding.karpov.invitation;
+
+/**
+ * Created by arsenitykarpov on 12/01/15.
+ */
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.widget.FrameLayout;
+
+import java.io.Serializable;
+
+public class OverlappingScreen extends Fragment {
+
+    public static final OverlappingScreen newInstance(
+            InformationScreenGenerator informationScreenGenerator) {
+        OverlappingScreen overlappingInformationScreen
+                = new OverlappingScreen();
+        overlappingInformationScreen.setInformationScreenGenerator(informationScreenGenerator);
+        overlappingInformationScreen.setRetainInstance(true);
+        return overlappingInformationScreen;
+    }
+
+    private InformationScreenGenerator mInformationScreenGenerator;
+
+    private ClosingLayout mAttachedView;
+
+    public void setInformationScreenGenerator(
+            InformationScreenGenerator informationScreenGenerator) {
+        mInformationScreenGenerator = informationScreenGenerator;
+    }
+
+    @Override
+    public void onStart() {
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.format = PixelFormat.TRANSLUCENT;
+        if (mAttachedView == null) {
+            mAttachedView = new ClosingLayout(getActivity());
+        }
+        mAttachedView.addView(mInformationScreenGenerator.getView(getActivity(), this));
+        ((WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE))
+                .addView(mAttachedView, layoutParams);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1f);
+        alphaAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        alphaAnimation.setDuration(300);
+        mAttachedView.getChildAt(0).startAnimation(alphaAnimation);
+        super.onStart();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return null;
+    }
+
+    public void show(FragmentManager fragmentManager) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(this, OverlappingScreen.class.getName());
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAttachedView != null) {
+            ((WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE))
+                    .removeView(mAttachedView);
+            mAttachedView = null;
+        }
+    }
+
+    public void detach() {
+        if (mAttachedView != null) {
+            ((WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE))
+                    .removeView(mAttachedView);
+            mAttachedView = null;
+        }
+        getFragmentManager().beginTransaction().remove(this).commitAllowingStateLoss();
+    }
+
+    private class ClosingLayout extends FrameLayout {
+
+        public ClosingLayout(Context context) {
+            super(context);
+        }
+
+        public ClosingLayout(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public ClosingLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+        }
+
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        public ClosingLayout(Context context, AttributeSet attrs, int defStyleAttr,
+                int defStyleRes) {
+            super(context, attrs, defStyleAttr, defStyleRes);
+        }
+
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent event) {
+            if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+                OverlappingScreen.this.detach();
+            }
+            return super.dispatchKeyEvent(event);
+        }
+    }
+
+    public static interface InformationScreenGenerator extends Serializable {
+
+        public View getView(Context context,
+                OverlappingScreen overlappingInformationScreen);
+    }
+}
